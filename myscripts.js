ce5e9c09692529f5598dc88ab7aa0f628fc07c2e
tedoonan <tedoonan@gmail.com>
2017-12-19 16:07:45 +1300

    Fix remove button and make order options restore to default

--- myscripts.js
+++ myscripts.js
@@ -5,8 +5,13 @@ var current_order_names = {};
 const VALUE_PRICE = 5;
 const TRADITIONAL_PRICE = 8;
 
-var current_size = 'medium';
-var current_base = 'medium;'
+const DEFAULT_SIZE = 'Medium';
+const DEFAULT_BASE = 'Classic';
+
+var current_size = DEFAULT_SIZE
+var current_base = DEFAULT_BASE
+var current_quantity = 1;
+var pizza_id_iterator = 0;
 
 function makePizzas(array, pizza_type) {
 
@@ -37,34 +42,47 @@ function getPizzaPrice(name) {
 function displayOrder() {
   var current_order = document.getElementById("current_order");
   current_order.innerHTML = '';
+
   var num_distinct_in_order = 0;
   var total_price = 0;
-  for (var pizza_name in current_order_names) {
-    if (current_order_names.hasOwnProperty(pizza_name)) {
-      num_distinct_in_order += 1;
-      var div = document.createElement('div');
-      var price = getPizzaPrice(pizza_name) * current_order_names[pizza_name];
-      total_price += price;
-      div.innerHTML = current_order_names[pizza_name] + ' x ' + pizza_name;
+  $.each(current_order_names, function( index, pizza) {
+    mypizza = current_order_names['Cheese0'];
+    num_distinct_in_order += 1;
+    var div = document.createElement('div');
+    var price = getPizzaPrice(pizza.name);
+    total_price += price;
+    div.innerHTML = pizza.quantity + ' x ' + pizza.name;
 
-      
+    div.className = "order_pizza";
+    div.id = index;
 
-      div.className = "order_pizza";
-      div.id = pizza_name;
+    price_span = document.createElement('span');
+    price_span.innerHTML = '$' + price;
+    price_span.className = "pizza_price";
+    div.appendChild(price_span);
 
-      price_span = document.createElement('span');
-      price_span.innerHTML = '$' + price;
-      price_span.className = "pizza_price";
-      div.appendChild(price_span);
+    remove_span = document.createElement('span');
+    remove_span.innerHTML = '&#x2716';
+    remove_span.className = "remove";
+    div.appendChild(remove_span);
 
-      remove_span = document.createElement('span');
-      remove_span.innerHTML = '&#x2716';
-      remove_span.className = "remove";
-      div.appendChild(remove_span);
-      document.getElementById("current_order").appendChild(div);
+    details_div = document.createElement('div');
 
-    }
-  }
+    size_span = document.createElement('span');
+    size_span.innerHTML = "Size: " + pizza.size;
+    size_span.id = 'size_order';
+    details_div.appendChild(size_span);
+
+    base_span = document.createElement('span');
+    base_span.innerHTML = "Base: " + pizza.base;
+    base_span.id = "base_order";
+    details_div.appendChild(base_span);
+
+    div.appendChild(details_div);
+
+    document.getElementById("current_order").appendChild(div);
+  });
+
   document.getElementById("total_price").innerHTML = "Total: $" + total_price;
   var new_height = num_distinct_in_order * 66 + 100;
   //document.getElementById("order").style.height = new_height + 'px';
@@ -73,11 +91,22 @@ function displayOrder() {
 }
 
 function addPizza(name) {
-  if (name in current_order_names) {
-    current_order_names[name] += 1;
-  } else {
-    current_order_names[name] = 1;
-  }
+  var pizza = new Object();
+  pizza.name = name;
+  pizza.size = current_size;
+  pizza.base = current_base;
+  pizza.quantity = current_quantity;
+  //current_order_names.push(pizza);
+  current_order_names[name + pizza_id_iterator] = pizza;
+  my = current_order_names[name + pizza_id_iterator];
+
+  pizza_id_iterator += 1;
+  restoreOrderDefaults();
+  //if (name in current_order_names) {
+    //current_order_names[name] += 1;
+ // } else {
+    //current_order_names[name] = 1;
+  //}
 }
 
 function removePizza(name) {
@@ -113,6 +142,19 @@ function closePopUp(el) {
   document.body.style.overflowY = "scroll"
 }
 
+function restoreOrderDefaults() {
+  current_size = DEFAULT_SIZE;
+  current_base = DEFAULT_BASE;
+  current_quantity = 1;
+
+  //restore order details ui
+  $('.size').removeClass("preview");
+  $("#" + DEFAULT_SIZE).addClass("preview");
+
+  $('.base').removeClass("preview");
+  $("#" + DEFAULT_BASE).addClass("preview");
+}
+
 $(document).ready(function(){
   $('.pizza_type').hide()
   $('.pizza_type[data-link=value]').show();
@@ -145,10 +187,10 @@ $(document).ready(function(){
     current_size = event.target.id;
   });
 
-  $('.crust').click(function(event) {
-    $('.crust').not(this).removeClass("preview");
+  $('.base').click(function(event) {
+    $('.base').not(this).removeClass("preview");
     $(this).addClass("preview");
-    current_crust = event.target.id;
+    current_base = event.target.id;
   });
 
   $('.add').click(function(event) {
@@ -158,10 +200,12 @@ $(document).ready(function(){
 
   $('.cancel_button').click(function(event) {
     closePopUp('order_popup');
+    restoreOrderDefaults();
   });
 
   $('.accept_button').click(function(event) {
     closePopUp('order_popup');
+    
     var pizza_name = document.getElementById("popup_name").innerHTML
     addPizza(pizza_name);
     displayOrder();
@@ -174,6 +218,22 @@ $(document).ready(function(){
 
   $('body').on('mouseover', '.pizza', function(event) {
     $(this).append( )
+  });
+
+  $('#plus').click(function () {
+    $('#quantity').html(function (y, val) {
+        var new_val = val * 1 + 1;
+        current_quantity = new_val;
+        return new_val;
+    });
+  });
+
+  $('#minus').click(function () {
+    $('#quantity').html(function (y, val) {
+        var new_val = val * 1 - 1;
+        current_quantity = new_val;
+        return new_val;
+    });
   });
 
 
