1e0d41c17a38a79191cfab7805c958436d9fc140
tedoonan <tedoonan@gmail.com>
2018-01-02 16:59:44 +1300

    Improve Code
    Various fixes to code and some UI changes

--- myscripts.js
+++ myscripts.js
@@ -1,7 +1,7 @@
 var value_pizza_names = ['Pepperoni', 'Hawaiian', 'Vegetarian', 'Cheese', 'Ham and Cheese']
-var traditional_pizza_names = ['Meat Lovers', 'Supreme Vegetarian', 'Wedge', 'Double Bacon', 'Double Meat and Cheese', 'BBQ Bacon', 'Italian', 'Triple Cheese', 'g', 'd', 's,', 'a']
+var traditional_pizza_names = ['Meat Lovers', 'Supreme Vegetarian', 'Wedge', 'Double Bacon', 'Meat and Cheese', 'BBQ Bacon', 'Italian', 'Triple Cheese', 'Left Beef']
 var gourmet_pizza_names = ['Ultimate Meat Lovers', 'BBQ Chicken', 'Supreme Chicken', 'Apricot Chicken', 'Spicy Chicken']
-//var current_order_names = [];
+
 var current_order_names = {};
 const VALUE_PRICE = 5;
 const TRADITIONAL_PRICE = 8;
@@ -16,21 +16,22 @@ var current_base = DEFAULT_BASE
 var current_quantity = 1;
 var pizza_id_iterator = 0;
 
-function makePizzas(array, pizza_type) {
+
+function displayMenu(array, pizza_type) {
   for(var i = 0; i < array.length; i++) {
     var div = document.createElement('div');
     div.innerHTML = array[i];
-    div.className = "pizza";
+    div.className = 'pizza';
     div.id = array[i];
 
     price_div = document.createElement('div');
     price_div.innerHTML = '$' + getPizzaPriceFromName(array[i]);
-    price_div.className = "pizza_price_menu";
+    price_div.className = 'pizza_price_menu';
     div.appendChild(price_div);
 
     add_div = document.createElement('div');
     add_div.innerHTML = 'Add to Order';
-    add_div.className = "add button";
+    add_div.className = 'add button';
     div.appendChild(add_div);
     document.getElementById(pizza_type).appendChild(div);
 
@@ -53,25 +54,8 @@ function getPizzaPriceFromName(name, size) {
   return price;
 }
 
-/*
-function getPizzaPriceFromType(type, size) {
-  var price;
-
-  if (type == 'value') {
-    price = VALUE_PRICE;
-  } else if (type === 'traditional') {
-    price = TRADITIONAL_PRICE;
-  } else {
-    price = GOURMET_PRICE;
-  }
-  if (size === 'Large') {
-    price += LARGE_EXTRA_PRICE;
-  }
-  return price;
-}*/
-
 function displayOrder() {
-  var current_order = document.getElementById("current_order");
+  var current_order = document.getElementById('current_order');
   current_order.innerHTML = '';
 
   var num_distinct_in_order = 0;
@@ -84,36 +68,37 @@ function displayOrder() {
     total_price += price;
     div.innerHTML = pizza.quantity + ' x ' + pizza.name;
 
-    div.className = "order_pizza";
+    div.className = 'order_pizza';
     div.id = index;
 
     price_span = document.createElement('span');
     price_span.innerHTML = '$' + price;
-    price_span.className = "pizza_price";
+    price_span.className = 'pizza_price';
     div.appendChild(price_span);
 
     remove_span = document.createElement('span');
     remove_span.innerHTML = '&#x2716';
-    remove_span.className = "remove";
+    remove_span.className = 'remove';
     div.appendChild(remove_span);
 
     details_div = document.createElement('div');
 
     size_span = document.createElement('span');
-    size_span.innerHTML = "Size: " + pizza.size;
+    size_span.innerHTML = 'Size: ' + pizza.size;
     size_span.id = 'size_order';
     details_div.appendChild(size_span);
 
     base_span = document.createElement('span');
-    base_span.innerHTML = "Base: " + pizza.base;
-    base_span.id = "base_order";
+    base_span.innerHTML = 'Base: ' + pizza.base;
+    base_span.id = 'base_order';
     details_div.appendChild(base_span);
 
     div.appendChild(details_div);
 
-    document.getElementById("current_order").appendChild(div);
+    document.getElementById('current_order').appendChild(div);
   });
 
+  //Hide place order button if order is empty
   if (num_distinct_in_order == 0) {
     document.getElementById('place_button').style.display = 'none';
   }
@@ -121,11 +106,11 @@ function displayOrder() {
     document.getElementById('place_button').style.display = 'block';
   }
 
-  document.getElementById("total_price").innerHTML = "Total: $" + total_price;
+  document.getElementById('total_price').innerHTML = 'Total: $' + total_price;
+
+  //Change height of order box to match size of order
   var new_height = num_distinct_in_order * 66 + 100;
-  //document.getElementById("order").style.height = new_height + 'px';
-  $('.order').animate({height:new_height},250);
-  //$('.order').fadeIn('slow');
+  $('#order').animate({height:new_height},250);
 }
 
 function addPizza(name) {
@@ -134,17 +119,11 @@ function addPizza(name) {
   pizza.size = current_size;
   pizza.base = current_base;
   pizza.quantity = current_quantity;
-  //current_order_names.push(pizza);
   current_order_names[name + pizza_id_iterator] = pizza;
   my = current_order_names[name + pizza_id_iterator];
 
   pizza_id_iterator += 1;
   restoreOrderDefaults();
-  //if (name in current_order_names) {
-    //current_order_names[name] += 1;
- // } else {
-    //current_order_names[name] = 1;
-  //}
 }
 
 function removePizza(name) {
@@ -155,29 +134,29 @@ function removePizza(name) {
       delete current_order_names[name];
     }
   } else {
-    alert('cant remove pizza which isnt in current order');
+    alert("cant remove pizza which isn't in current order");
   }
 }
 
 //http://www.dynamicdrive.com/forums/showthread.php?17849-How-to-grey-out-the-html-page-but-pop-up-a-interactive-dialog-box
 
 function showPopUp(el) {
-  var cvr = document.getElementById("cover")
+  var cvr = document.getElementById('cover')
   var dlg = document.getElementById(el)
-  cvr.style.display = "block"
-  dlg.style.display = "block"
-  if (document.body.style.overflow = "hidden") {
-    cvr.style.width = "1024"
-    cvr.style.height = "100&#37;"
+  cvr.style.display = 'block'
+  dlg.style.display = 'block'
+  if (document.body.style.overflow = 'hidden') {
+    cvr.style.width = '1024'
+    cvr.style.height = '100&#37;'
   }
 }
 
 function closePopUp(el) {
-  var cvr = document.getElementById("cover")
+  var cvr = document.getElementById('cover')
   var dlg = document.getElementById(el)
-  cvr.style.display = "none"
-  dlg.style.display = "none"
-  document.body.style.overflowY = "scroll"
+  cvr.style.display = 'none'
+  dlg.style.display = 'none'
+  document.body.style.overflowY = 'scroll'
 }
 
 function restoreOrderDefaults() {
@@ -186,68 +165,57 @@ function restoreOrderDefaults() {
   current_quantity = 1;
 
   //restore order details ui
-  $('.size').removeClass("preview");
-  $("#" + DEFAULT_SIZE).addClass("preview");
+  $('.size').removeClass('preview');
+  $('#' + DEFAULT_SIZE).addClass('preview');
 
-  $('.base').removeClass("preview");
-  $("#" + DEFAULT_BASE).addClass("preview");
-  $('#quantity').html("1");
+  $('.base').removeClass('preview');
+  $('#' + DEFAULT_BASE).addClass('preview');
+  $('#quantity').html('1');
 }
 
 $(document).ready(function(){
   $('.pizza_type').hide()
   $('.pizza_type[data-link=value]').show();
 
-  $('.links').mouseover(function() {
-    $(this).addClass("preview");
-  });
-
-	$('.links').mouseleave(function() {
-    $(this).removeClass("preview");
-  });
-
-
-
-
-  makePizzas(value_pizza_names, 'value_pizza_type');
-  makePizzas(traditional_pizza_names, 'traditional_pizza_type');
-  makePizzas(gourmet_pizza_names, 'gourmet_pizza_type');
+  displayMenu(value_pizza_names, 'value_pizza_type');
+  displayMenu(traditional_pizza_names, 'traditional_pizza_type');
+  displayMenu(gourmet_pizza_names, 'gourmet_pizza_type');
 
 
   $('.links').click(function() {
-    $('.links').not(this).removeClass("active");
-  	$(this).addClass("active");
+    $('.links').not(this).removeClass('active');
+  	$(this).addClass('active');
     $('.pizza_type').hide();
     var pizza_name = $(this).data('link');
     $('.pizza_type[data-link=' + pizza_name + ']').show();
   });
 
   $('.size').click(function(event) {
-    $('.size').not(this).removeClass("preview");
-    $(this).addClass("preview");
+    $('.size').not(this).removeClass('preview');
+    $(this).addClass('preview');
     current_size = event.target.id;
   });
 
   $('.base').click(function(event) {
-    $('.base').not(this).removeClass("preview");
-    $(this).addClass("preview");
+    $('.base').not(this).removeClass('preview');
+    $(this).addClass('preview');
     current_base = event.target.id;
   });
 
   $('.add').click(function(event) {
     showPopUp('order_popup');
-    document.getElementById("popup_name").innerHTML=event.target.parentNode.id;
+    document.getElementById('popup_name').innerHTML=event.target.parentNode.id;
   });
 
-  $('.cancel_button').click(function(event) {
+  $('#cancel_button').click(function(event) {
     closePopUp('order_popup');
     restoreOrderDefaults();
   });
 
-  $('.accept_button').click(function(event) {
+  $('#accept_button').click(function(event) {
     closePopUp('order_popup');
     
-    var pizza_name = document.getElementById("popup_name").innerHTML
+    var pizza_name = document.getElementById('popup_name').innerHTML
     addPizza(pizza_name);
     displayOrder();
   });
@@ -255,10 +223,6 @@ $(document).ready(function(){
   $('body').on('click', '.remove', function(event) {
     removePizza(event.target.parentNode.id);
     displayOrder();
-  });
-
-  $('body').on('mouseover', '.pizza', function(event) {
-    $(this).append( )
   });
 
   $('#plus').click(function () {
@@ -281,13 +245,4 @@ $(document).ready(function(){
         }
     });
   });
-    var increased = parseInt($(this).parent().find('.number').text());
-    if (isNaN(increased) || increased > 0) {
-        $(this).parent().find('.number').html(function (t, val) {
-            return val * 1 - 1
-        });
-    } else {
-      return false;
-    }
-
 });
